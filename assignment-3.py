c7812fe454875bc5a62d7f68ce62d0450a0cd6f1
nelsonbunani <n.bunani@yahoo.com>
2015-07-15 09:40:16 +0300

    assignment3

---
+++ assignment-3.py
@@ -0,0 +1,38 @@
+#
+grade=input("input grade: ")
+gradeA=input("input gardeA: ")
+gradeB=input("input gradeB: ")
+gradeC=input("input gradeC: ")
+gradeO=input("input gradeO: ")
+radeF=input("input gradeF: ")
+if gradeA>=70:
+	print("A")
+else:
+	print("B")
+if gradeB<70>60:
+	print("B")
+else:
+	print("C")
+
+if gradeC<60>55:
+	print("C")
+else:
+	print("D")
+
+if gradeD<55>50:
+	print("D")
+else:
+	print("O")
+if gradeO<50>40:
+	print("O")
+else:
+	print("F")
+
+# elif paper1<70:
+# 	print("F")
+# paper2=input("input paper2: ")
+# if paper2==60:
+# 	print("D")
+# elif paper2<60:
+# 	print("O")
+
