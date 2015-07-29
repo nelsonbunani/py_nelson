fc4371f89ea29b9f965d248ba60a27d843e2d469
nelsonbunani <n.bunani@yahoo.com>
2015-07-29 11:23:54 +0300

    my file assignment

---
+++ file2_assignment.py
@@ -0,0 +1,42 @@
+#A file function that read a file
+marks=[]
+def open_file():
+file_name=open(C:\Users\DI0160\Documents\py_nelson\marks.txt,"r")
+marks=file_name.readlines()
+return marks
+
+#grade for each mark in the list
+def_grade(marks_list)
+if marks_list>80:
+	grade="A"
+	points=6
+elif marks_list<80 and marks>75:
+	gade="B"
+elif marks_list<75 and marks>60:
+	grade="C"
+elif marks_list<60 and marks>55:
+	grade="D"
+elif marks_list<55 and marks>45:
+	grade="E"
+elif marks_list<45 and marks>40:
+	grade="O"
+else grade=FAIL
+return[grade,marks]
+pass
+
+# computes average of grades or marks
+def average(total,no)
+	y=total(t)/no
+	t=0
+	return t
+
+	# main function that puts all functions together
+def main():	
+    data = open_file()
+    mark
+ 	mark=[]
+    marks=[]
+    names=[]
+    grade=[]
+    
+    
